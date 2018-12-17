367ade4c2b3e8ce72c36b3c252a141901d7c9282
Othman Mejdoubi <o.mejdoubi@gmail.com>
2018-12-17 20:13:55 +0100

    resolve mongoose deprecation warning

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
 module.exports = {
   mongoose
